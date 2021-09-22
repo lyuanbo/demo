29017f6fe087a7ad5c3e408bc32983b73d686865
GitHub Action <action@github.com>
2021-09-22 06:29:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/lyuanbo/demo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lyuanbo/demo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lyuanbo.demo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lyuanbo.demo.MyBundle
 
 class MyApplicationService {
 
